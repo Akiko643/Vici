3ef6a8a5b4fdeae567e2cf867807156d2929c04e
n.tamikan <n.tamikan@gmail.com>
2021-06-12 18:10:10 +0800

    interview first

--- src/Component/Interview/Interview.js
+++ src/Component/Interview/Interview.js
@@ -2,14 +2,35 @@ import React from "react";
 import defaultBgImage from "../../Img/INTERVIEW.png";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+// import Carousel from "react-multi-carousel";
 import Carousel from "./Carousel";
+import "react-multi-carousel/lib/styles.css";
+import { CarouselData } from "./CarouselData";
+import "./Interview.css";
 // import Feed from "./Feed";
 
 const Interview = () => {
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+            slidesToSlide: 2, // optional, default to 1.
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+            slidesToSlide: 2, // optional, default to 1.
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 2, // optional, default to 1.
+        },
+    };
     return (
         <div>
             <Navbar />
-            <Carousel />
+            {/* <Carousel slides={CarouselData} />; */}
             {/* <Feed /> */}
             <Footer />
         </div>
