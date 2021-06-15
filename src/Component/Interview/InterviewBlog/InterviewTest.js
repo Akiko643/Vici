8a6aaaffe3878141c7b65c3f8bbb4f97aef8376b
n.tamikan <n.tamikan@gmail.com>
2021-06-16 01:45:59 +0800

    int blog :D

--- src/Component/Interview/InterviewBlog/InterviewTest.js
+++ src/Component/Interview/InterviewBlog/InterviewTest.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useRouteMatch, useHistory } from 'react-router';
-import { useCol, useDoc } from '../../../Hooks/firebase';
-import './InterviewBlog.css';
-import ReactPlayer from 'react-player';
-import PlayerData from './PlayerData.js';
+import React, { useEffect, useState } from "react";
+import { useRouteMatch, useHistory } from "react-router";
+import { useCol, useDoc } from "../../../Hooks/firebase";
+import "./InterviewTest.css";
+import ReactPlayer from "react-player/youtube";
+import PlayerData from "./PlayerData.js";
+import Navbar from "../../Navbar/Navbar";
+import Back from "../../../Img/vector__back.svg";
+import Card from "../Card";
+import Footer from "../../Footer/Footer";
 export const InterviewTest = (props) => {
     let match = useRouteMatch();
-    const { updateRecord } = useCol('/content/contents/Blog');
+    const { updateRecord } = useCol("/content/contents/Blog");
     const doc = useDoc(`/content/contents/Blog/${match.params.blogId}`);
     const [blogData, setBlogData] = useState({});
     const history = useHistory();
@@ -14,85 +18,113 @@ export const InterviewTest = (props) => {
     const toTime = (timestamp) => {
         var date = new Date(timestamp?.seconds * 1000);
         return (
-            date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear()
+            date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear()
         );
     };
-    const [first, setFirst] = useState(true);
-    useEffect(() => {
-        setBlogData(doc?.data);
-        if (doc?.data?.visits != undefined && first) {
-            doc?.updateRecord({ visits: doc?.data?.visits + 1 });
-            setFirst(false);
-        }
-    }, [doc?.data]);
+    const card = {
+        image: "https://s2.im.ge/2021/06/13/QDbDY.png",
+        header: "John Doe",
+        subheader: "Director at Famous Company",
+    };
     return (
-        <div className='blog-container w100 flex-center'>
-            <div className='blog-mid-container'>
-                <div className='flex-row breadcrumb'>
-                    <p onClick={() => history.push('/')} className='pointer'>
-                        Home
-                    </p>
-                    /
-                    <p
-                        onClick={() => history.push('/blog')}
-                        className='pointer'
-                    >
-                        Blog
-                    </p>
-                    /
-                    <p
-                        onClick={() =>
-                            history.push(`/blog/${blogData?.category?.name}`)
-                        }
-                        className='pointer'
-                    >
-                        {blogData?.category?.name}
-                    </p>
-                    /<p>{blogData?.header}</p>
+        <div>
+            <Navbar />
+            <div className="interview__container pa-vw-10 pr">
+                <img src={Back} className="arrow" />
+                <div className="player-wrapper">
+                    <ReactPlayer
+                        className="react-player"
+                        url={PlayerData.src}
+                        // width="100%"
+                        // height="100%"
+                    />
                 </div>
-                <h1 className='fs-40'>{blogData?.header}</h1>
-                <img src={blogData?.image} className='heading-image' />
-                <ReactPlayer src={PlayerData.src} />
-                <div className='flex flex-wrap'>
-                    <div className='flex-col handalt'>
-                        <div className='flex-row mb-5 items-center'>
+                <div className="upper__text mt-80">
+                    <h1 className="int__title">John Doe</h1>
+                    <h2 className="int__subtitle">
+                        Director at Famous Company
+                    </h2>
+                    <div className="little__text mt-40">
+                        <p className="publisher__name ">
                             <img
-                                src={data?.profilePicUrl}
+                                src="https://lh3.googleusercontent.com/proxy/6Ab67PmK5LojafyV-gshTqBi02Yy0p4AXDSv7YDtlIec9EVn8-G2C_F_OXbWeRLN361f609pXhNBo8MRfFltdexO"
                                 alt={data?.displayName}
-                                className='publisher-profile-img'
+                                className="publisher-profile-img w-15 h-15 mr-5"
                             />
-                            <div className='flex flex-col'>
-                                <div className='c-smallheading side-heading'>
-                                    Puplisher:
-                                </div>
-                                <p
-                                    onClick={() => {}}
-                                    className='publisher-name'
-                                >
-                                    {data?.displayName}
-                                </p>
-                            </div>
-                        </div>
-                        <div className='flex flex-col'>
-                            <div className='c-smallheading side-heading'>
-                                Хандалт:
-                            </div>
-                            <p className='side-accessed'>{blogData?.visits}</p>
-                        </div>
-                        <div className='flex flex-col'>
-                            <div className='c-smallheading side-heading'>
-                                Date:
-                            </div>
-                            <p className='side-date'>
-                                {toTime(blogData?.createdAt)}
-                            </p>
-                        </div>
+                            Publisher Name
+                        </p>
+                        <p className="date__created">
+                            {toTime(blogData?.createdAt)}
+                        </p>
                     </div>
-                    <div className='blog-content'>
-                        <p>{blogData?.text}</p>
-                    </div>
+                    <hr className="mt-30" />
+                </div>
+                <div className="big-text w-vw-66 mt-30">
+                    <p>
+                        The brain doesn't directly communicate with the world.
+                        It experiences it from picking it up by our sensory
+                        receptors. Our 5 senses pick up everything around us;
+                        the smell of perfume, the sound of music, the feeling of
+                        fur, the sight of sunsets, the taste of food. Whenever
+                        we experience the 5 senses it goes through mental
+                        processes called sensation and perception.{" "}
+                    </p>
+
+                    <p>
+                        Sensation - a process of when sensory receptors receive
+                        information and send neural signals to our brain.
+                    </p>
+
+                    <p>
+                        Perception - a process that receives the sensory
+                        information and interprets, organizes, and experiences
+                        it.{" "}
+                    </p>
+                    <br />
+                    <p>What is sensory adaptation? </p>
+
+                    <p>
+                        Whenever you paint your nails, there's a very strong
+                        smell of chemicals. But after a while the smell isn't as
+                        noticeable as it was before. It didn't go away, it just
+                        decreased in sensitivity due to constant stimulation.
+                        This is called sensory adaptation.{" "}
+                    </p>
+                    <br />
+                    <p>How do you measure the senses? </p>
+
+                    <p>
+                        You can measure the senses by measuring the two
+                        thresholds and applying the signal detection theory.
+                        There are two thresholds: the Absolute Threshold and the
+                        Difference Threshold (also known as Just Noticeable
+                        Difference).{" "}
+                    </p>
+
+                    <p>
+                        Absolute Threshold - The smallest amount of stimulus
+                        that can be detected half of the time.
+                    </p>
+
+                    <p>
+                        Difference Threshold - The smallest amount of stimulus
+                        that can be changed and the difference can be detected
+                        half of the time. The signal detection theory states
+                        that the sensation depends on the characteristics of the
+                        stimulus, the background stimulus, and the detector
+                        itself. This theory is used to predict when a weak
+                        signal will be detected.
+                    </p>
+                </div>
+                <hr className="mt-45 w-vw-66" />
+                <h1 className="latest__speakers  mt-40">Latest Speakers</h1>
+                <div className="w-vw-66 mt-40 lower__container">
+                    <Card card={card} index={1} />
+                    <Card card={card} index={2} />
+                    <Card card={card} index={3} />
                 </div>
             </div>
+            <Footer />
         </div>
     );
 };
--- src/Component/Interview/InterviewBlog/PlayerData.js
+++ src/Component/Interview/InterviewBlog/PlayerData.js
@@ -1,9 +1,8 @@
-const PlayerData = [
-    {
-        src: "https://youtu.be/y2u_-QomGXU",
-        title: "Just the Two of Us",
-        subtitle: "Lucy Ellis",
-        content:
-            "I see the red lights in her room This girl sings until its noon It's like she's singing to the moon I see the sparkles in her eyes This girl is always on my mind She leaves me star struck all the time",
-    },
-];
+const PlayerData = {
+    src: "https://youtu.be/y2u_-QomGXU",
+    title: "Just the Two of Us",
+    subtitle: "Lucy Ellis",
+    content:
+        "I see the red lights in her room This girl sings until its noon It's like she's singing to the moon I see the sparkles in her eyes This girl is always on my mind She leaves me star struck all the time",
+};
+export default PlayerData;
