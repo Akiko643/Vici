4e30409cdce963bcd443a777711d43faa7c36717
n.tamikan <n.tamikan@gmail.com>
2021-06-15 17:59:12 +0800

    .

--- src/Component/Interview/Card.js
+++ src/Component/Interview/Card.js
@@ -8,18 +8,22 @@ const Card = ({ card, index }) => {
     };
     return (
         <div>
-            <div key={index} className="card">
+            <div
+                key={index}
+                className="card"
+                onMouseEnter={handleHover}
+                onMouseLeave={handleHover}
+            >
                 <img src={card.image} alt={card.header} />
                 <Fade bottom collapse when={show}>
                     <h2 className="feed-subtitle">{card.subheader}</h2>
                 </Fade>
-                <h2
-                    className="feed-title"
-                    onMouseEnter={handleHover}
-                    onMouseLeave={handleHover}
-                >
-                    {card.header}
-                </h2>
+
+                <Fade bottom collapse when={show}>
+                    <h2 className={show ? "feed-title active" : "feed-title"}>
+                        {card.header}
+                    </h2>
+                </Fade>
             </div>
         </div>
     );
