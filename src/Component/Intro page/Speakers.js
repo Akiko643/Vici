245799b832d99c460ad9571c135e3b18a3d344ba
n.tamikan <n.tamikan@gmail.com>
2021-06-16 18:28:39 +0800

    just comment

--- src/Component/Intro page/Speakers.js
+++ src/Component/Intro page/Speakers.js
@@ -1,17 +1,3 @@
-// import react from 'react'
-
-// function Speakers({person}) {
-//     return(
-//         <div className="pr mt-20">
-//             <div className="b-white w-270 h-390 bradius-10 z--1"></div>
-//             <img className="absolute b-0 l-0 z-0" src={`./images/${person}.png`}></img>
-//             <h3 className="absolute b-0 h-40 w-160 b-default z-1 c-white flex items-center justify-center bradius-5">{person}</h3>
-//         </div>
-//     )
-// }
-
-// export default Speakers;
-
 import react from "react";
 import Card from "../Interview/Card";
 const Speakers = () => {
