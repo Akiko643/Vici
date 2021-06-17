d25c3b24e702cead5eba58914a0039ef8121eff7
enhuush_enhmunh <enhuush0704@gmail.com>
2021-06-17 15:12:21 +0800

    interview

--- src/Component/Interview/Carousel.js
+++ src/Component/Interview/Carousel.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import Img from "../../Img/article.png";
 import "./Carousel.css";
-import { CarouselData } from "./CarouselData.js";
 import rightIcon from "../../Img/RightArrow.svg";
 import leftIcon from "../../Img/LeftArrow.svg";
 import ReactPlayer from "react-player";
+import { useHistory } from "react-router-dom";
 const Carousel = ({ slides }) => {
     const [current, setCurrent] = useState(0);
     const length = slides.length;
+    const history = useHistory();
     const nextSlide = () => {
         setCurrent(current === length - 1 ? 0 : current + 1);
     };
@@ -31,27 +32,35 @@ const Carousel = ({ slides }) => {
                     onClick={nextSlide}
                 />
 
-                {CarouselData.map((slide, index) => {
+                {slides.map((slide, index) => {
                     return (
                         <div
                             className={
                                 index === current ? "slide active" : "slide"
                             }
+                            onClick={() => {
+                                history.push(`/interview/${slide.id}`); 
+                                window.scroll({
+                                    top: 0,
+                                    left: 0,
+                                    behavior: 'smooth'
+                                });
+                            }}
                             key={index}
                         >
                             {index === current && (
                                 <div className="vignette">
                                     <img
-                                        src={slide.image}
-                                        alt={slide.header}
+                                        src={slide?.image}
+                                        alt={slide?.header}
                                         className="image"
                                     />
 
                                     {/* <ReactPlayer url="https://youtu.be/y2u_-QomGXU"/> */}
                                 </div>
                             )}
-                            <h2 className="car-title">{slide.header}</h2>
-                            <h2 className="car-subtitle">{slide.subheader}</h2>
+                            <h2 className="car-title">{slide?.header}</h2>
+                            <h2 className="car-subtitle">{slide?.subheader}</h2>
                         </div>
                     );
                 })}
