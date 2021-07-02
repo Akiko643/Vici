e07310e34e6abb6ad19f52ca348ed223955fc5e3
n.tamikan <n.tamikan@gmail.com>
2021-07-02 22:09:00 +0800

    swipeable responsive interview ver 1 (not tested)

--- src/Component/Interview/Carousel.js
+++ src/Component/Interview/Carousel.js
@@ -5,6 +5,7 @@ import rightIcon from "../../Img/RightArrow.svg";
 import leftIcon from "../../Img/LeftArrow.svg";
 import ReactPlayer from "react-player";
 import { useHistory } from "react-router-dom";
+import { useSwipeable } from "react-swipeable";
 const Carousel = ({ slides }) => {
     const [current, setCurrent] = useState(0);
     const length = slides.length;
@@ -15,56 +16,50 @@ const Carousel = ({ slides }) => {
     const prevSlide = () => {
         setCurrent(current === 0 ? length - 1 : current - 1);
     };
+    const mobileHandle = useSwipeable({
+        onSwipedLeft: nextSlide,
+        onSwipedRight: prevSlide,
+    });
     if (!Array.isArray(slides) || slides.length <= 0) {
         return null;
     }
     return (
-        <div className="big-container ">
-            <div className="carousel-container mt-60">
-                <img
-                    src={leftIcon}
-                    className="left-arrow"
-                    onClick={prevSlide}
-                />
-                <img
-                    src={rightIcon}
-                    className="right-arrow"
-                    onClick={nextSlide}
-                />
+        <div className="carousel-container mt-60" {...mobileHandle}>
+            <img src={leftIcon} className="left-arrow" onClick={prevSlide} />
+            <img src={rightIcon} className="right-arrow" onClick={nextSlide} />
 
-                {slides.map((slide, index) => {
-                    return (
-                        <div
-                            className={
-                                index === current ? "slide active" : "slide"
-                            }
-                            onClick={() => {
-                                history.push(`/interview/${slide.id}`); 
-                                window.scroll({
-                                    top: 0,
-                                    left: 0,
-                                    behavior: 'smooth'
-                                });
-                            }}
-                            key={index}
-                        >
-                            {index === current && (
-                                <div className="vignette">
-                                    <img
-                                        src={slide?.image}
-                                        alt={slide?.header}
-                                        className="image"
-                                    />
+            {slides.map((slide, index) => {
+                return (
+                    <div
+                        className={index === current ? "slide active" : "slide"}
+                        onClick={() => {
+                            history.push(`/interview/${slide.id}`);
+                            window.scroll({
+                                top: 0,
+                                left: 0,
+                                behavior: "smooth",
+                            });
+                        }}
+                        key={index}
+                    >
+                        {index === current && (
+                            <div className="vignette">
+                                <img
+                                    src={slide?.image}
+                                    alt={slide?.header}
+                                    className="image"
+                                />
 
-                                    {/* <ReactPlayer url="https://youtu.be/y2u_-QomGXU"/> */}
-                                </div>
-                            )}
+                                {/* <ReactPlayer url="https://youtu.be/y2u_-QomGXU"/> */}
+                            </div>
+                        )}
+                        <div className="title__container">
                             <h2 className="car-title">{slide?.header}</h2>
                             <h2 className="car-subtitle">{slide?.subheader}</h2>
                         </div>
-                    );
-                })}
-            </div>
+                    </div>
+                );
+            })}
         </div>
     );
 };
